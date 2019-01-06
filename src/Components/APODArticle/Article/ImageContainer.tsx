77be14d136a000291845557d6dd509d69e20c873
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-01-06 11:20:12 -0300

    Added image container styles

--- src/Components/APODArticle/Article/ImageContainer.tsx
+++ src/Components/APODArticle/Article/ImageContainer.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
+import './ImageContainer.css';
+
+
 interface IImageContainerProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
     href: string;
 }
 
-function ImageContainer({ className, href, ...props }: IImageContainerProps) {
+function ImageContainer({ className, href, children, ...props }: IImageContainerProps) {
     const ownClassName = "image-container";
     return (
-        <a href={href}>
-            <div
-                className={className ? `${ownClassName} ${className}` : ownClassName}
-                {...props}
-            />
-        </a>
+        <div
+            className={className ? `${ownClassName} ${className}` : ownClassName}
+            {...props}
+        >
+            <a href={href}>
+                {children}
+            </a>
+        </div>
     );
 }
 
