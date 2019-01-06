49c4d577839d98c3cea49b19e1626b80cf64deb5
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-01-06 13:39:28 -0300

    Added spinner styles

--- src/Components/Common/Spinner.tsx
+++ src/Components/Common/Spinner.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import './Spinner.css';
+
+
 function Spinner() {
-    return <div className="spinner" />
+    return (
+        <div className="spinner">
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div>
+    );
 }
 
 export default Spinner;
