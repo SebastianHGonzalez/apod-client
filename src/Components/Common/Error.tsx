e5909fe9de3e8d48c68ae0168002b1225e749acc
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2019-01-06 14:16:24 -0300

    Added error styles

--- src/Components/Common/Error.tsx
+++ src/Components/Common/Error.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import './Error.css';
+
 
 interface IErrorProps extends React.Props<any> {
     error: Error;
 }
 
-function Error({error}: IErrorProps) {
+function Error({ error }: IErrorProps) {
     return (
         <div className="error">
+            <strong>
+                Error:
+            </strong>
             {error.message}
         </div>
     );
